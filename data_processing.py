7f4e01347e7b7dd5771b94e284ee0ddf825c731a
22205groc <22205groc@cotham.bristol.sch.uk>
2026-07-16 11:09:34 +0100

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,7 @@
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 data_in = [["harold","dave","albert","danial","auther","ben","dylan"],[1,2,3,4,5,6,7],[1,2,3,4,5,6,7],[["bunking"],["sent out","bunking"],["bunking","bunking","late"],["rep","late","sent out","rep"],["sent out","late","late","rep","sent out"],["late","late","rep","rep","rep","rep"],["rep","rep","rep","late","late","sent out","late"]]] # names(str), Hp(int), Bp(int), Bp reasons(2d)(str)
 #sort data using bubble sort
-# Thomas' test
+# Thomas' test merge
 i = 0
 swaps = True
 while swaps == True:
